19a5682232b906bf0b7bc270c64eb3f584d569b1
Michel <sistemas@grandmom.com.br>
2025-07-04 12:04:22 -0300

    ajusta logo como link e espacamento rodape

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,7 +18,7 @@ export default async function Home() {
   });
   return resultado
   }
-  categorias.map((el) => (<></>))*/
+  categorias.map((el) => (<></>))/*/
 
   return (
     <div className="flex flex-col items-center justify-center gap-[25px]">
